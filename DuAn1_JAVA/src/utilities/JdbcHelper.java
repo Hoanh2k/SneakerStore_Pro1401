3349e9abac4f688f1cb9f13c521e56108ca8b85e
hoanh <112262585+Hoanh2k@users.noreply.github.com>
2024-03-15 16:42:09 +0700

    kn

--- DuAn1_JAVA/src/utilities/JdbcHelper.java
+++ DuAn1_JAVA/src/utilities/JdbcHelper.java
@@ -1,25 +1,33 @@
 package utilities;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.DriverManager;
 
 public class JdbcHelper {
     
-    public static Connection getConnection() {
+    public static Connection getConnection() throws SQLServerException {
 
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=Sneacker_Store";
-            String username = "sa";
-            String pass = "123456";
-            return DriverManager.getConnection(url, username, pass);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            String url = "jdbc:sqlserver://localhost:1433;databaseName=Sneacker_Store";
+//            String username = "sa";
+//            String pass = "123456";
+//            return DriverManager.getConnection(url, username, pass);
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+
+        SQLServerDataSource dataSource= new SQLServerDataSource();
+        dataSource.setUser("sa");
+        dataSource.setPassword("123456");
+        dataSource.setPortNumber(1433);
+        dataSource.setEncrypt(Boolean.FALSE);
+        dataSource.setDatabaseName("SneakerStore");
+        return dataSource.getConnection();
     }
-     public static void main(String[] args) {
+     public static void main(String[] args) throws SQLServerException {
         System.out.println(getConnection());
 
     }
