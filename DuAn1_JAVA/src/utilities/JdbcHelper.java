dd50c6db6ef9c6656d94a316b43f6e5a122e2729
letrunghieu07 <trunghieu07032003@gamil.com>
2024-03-16 22:38:06 +0700

    update code 16/3/2024

--- DuAn1_JAVA/src/utilities/JdbcHelper.java
+++ DuAn1_JAVA/src/utilities/JdbcHelper.java
@@ -1,25 +1,26 @@
 package utilities;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.DriverManager;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.*;
+
+
 
 public class JdbcHelper {
     
-    public static Connection getConnection() {
+    @SuppressWarnings("empty-statement")
+    
+    public static Connection getConnection() throws SQLServerException {
 
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            String url = "jdbc:sqlserver://localhost:1433;databaseName=SneakerStore";
-            String username = "sa";
-            String pass = "123456";
-            return DriverManager.getConnection(url, username, pass);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
+        SQLServerDataSource dataSource= new SQLServerDataSource();
+        dataSource.setUser("sa");
+        dataSource.setPassword("123456");
+        dataSource.setPortNumber(1433);
+        dataSource.setEncrypt(Boolean.FALSE);
+        dataSource.setDatabaseName("SneakerStore");
+        return dataSource.getConnection();
     }
-     public static void main(String[] args) {
+     public static void main(String[] args) throws SQLServerException {
         System.out.println(getConnection());
 
     }
